4b6f1e6f58ce12ecdf41534aa1f5212d8d9e5456
soundersfan36 <milesl25@lakesideschool.org>
2022-06-10 12:36:59 -0700

    Created simple OBJ file reader

---
+++ ObjReaderV1.py
@@ -0,0 +1,66 @@
+#
+# Reads obj file as text document and organizes the contents
+# into vertices, textures, and faces
+#
+filename = "dwayne.obj"
+path = "C:/Users/MilesL25/.spyder-py3/3D Files/" + filename
+file = open(path)
+data = []
+num = 0
+for line in file:
+    data.append(line)
+    num += 1
+
+
+vertices = []
+textures = []
+faces = []
+# Each line can start with one of these
+# or a '#' to mean comment, or other random stuff
+possible_starts = ['v', 'vp', 'vn', 'vt', 'f', 'l']
+for line in data:
+    # remove new lines at end of lines
+    if '\n' in line:
+        line = line[:-1]
+    # convert to list
+    nums = line.split(" ")
+    # some lines are empty just for formatting
+    if len(nums) <= 1:
+        continue
+    # character(s) at the beginning of the line tell us 
+    # what the line is
+    data_type = nums[0]
+    
+    # vertex (v x y z)
+    if data_type == 'v':
+        coords = nums[1:]
+        vertices.append(coords)
+    # texture (maps material to vertices, I think)
+    # (vt u v w), v and w are optional
+    elif data_type == 'vt':
+        texture = nums[1:]
+        textures.append(texture)
+    # face (f v1/vt1 v2/vt2 v3/vt3)
+    # can have more than 3, vts are optional and can include
+    # vn, which is a normal vector
+    elif data_type == 'f':
+        points = nums[1:]
+        # this splits up each point in the face into the vertex
+        # and the vertex texture
+        for i in range(len(points)):
+            p = points[i]
+            info = p.split("/")
+            points[i] = info
+        faces.append(points)
+
+# Now we have the three lists. The material of the faces, 
+# which has color, transparency, shininess, secondary color, etc
+# is in the material.lib file which has a similar formatting
+# but I gtg so I don't have time to parse it.
+
+print("Vertices: ", len(vertices))
+print("Textures: ", len(textures))
+print("Faces: ", len(faces))
+
+
+    
