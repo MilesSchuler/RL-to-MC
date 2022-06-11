f026ece440e4f6aa749e3a1b61ceff5f5183d554
soundersfan36 <milesl25@lakesideschool.org>
2022-06-11 10:07:19 -0700

    minor fix

--- ObjReaderV1.py
+++ ObjReaderV1.py
@@ -30,15 +30,14 @@ for line in data:
     # character(s) at the beginning of the line tell us 
     # what the line is
     data_type = nums[0]
-    
     # vertex (v x y z)
     if data_type == 'v':
-        coords = nums[1:]
+        coords = [float(i) for i in nums[1:]]
         vertices.append(coords)
     # texture (maps material to vertices, I think)
     # (vt u v w), v and w are optional
     elif data_type == 'vt':
-        texture = nums[1:]
+        texture = [float(i) for i in nums[1:]]
         textures.append(texture)
     # face (f v1/vt1 v2/vt2 v3/vt3)
     # can have more than 3, vts are optional and can include
@@ -50,7 +49,7 @@ for line in data:
         for i in range(len(points)):
             p = points[i]
             info = p.split("/")
-            points[i] = info
+            points[i] = [int(i) for i in info]
         faces.append(points)
 
 # Now we have the three lists. The material of the faces, 
@@ -58,9 +57,22 @@ for line in data:
 # is in the material.lib file which has a similar formatting
 # but I gtg so I don't have time to parse it.
 
+xmax = max([v[0] for v in vertices])
+xmin = min([v[0] for v in vertices])
+ymax = max([v[1] for v in vertices])
+ymin = min([v[1] for v in vertices])
+zmax = max([v[2] for v in vertices])
+zmin = min([v[2] for v in vertices])
+
+
+
 print("Vertices: ", len(vertices))
 print("Textures: ", len(textures))
 print("Faces: ", len(faces))
+print("X range: ", xmin, "-", xmax)
+print("Y range: ", ymin, "-", ymax)
+print("Z range: ", zmin, "-", zmax)
+file.close()
 
 
     
