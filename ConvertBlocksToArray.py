a4e56014c3b01403a4da4cee643693178fdc71ed
slickOnGit <milesdz@icloud.com>
2022-07-30 14:08:56 -0700

    Update ConvertBlocksToArray.py

--- ConvertBlocksToArray.py
+++ ConvertBlocksToArray.py
@@ -3,30 +3,27 @@ import numpy
 import glob
 import os
 
-def rgbValues(imageName: str):
-    im = Image.open("block/" + imageName, "r")
-    pixelValues = list(im.getdata())
-    blockRGBValues = numpy.array([], dtype = numpy.uint8)
-    for rgba in pixelValues:
-        blockRGBValues = numpy.append(blockRGBValues, rgba[0])
-        blockRGBValues = numpy.append(blockRGBValues, rgba[1])
-        blockRGBValues = numpy.append(blockRGBValues, rgba[2])
-    return blockRGBValues
-
-def addToClipBoard(text):
-    command = 'echo ' + text.strip() + '| clip'
-    os.system(command)
-
-blocks = numpy.empty((0, 0), dtype = numpy.uint8)
-
-directory = "./block"
-for filename in glob.iglob(f'{directory}/*'):
-    if filename.endswith(".png"):
-        values = rgbValues(filename[8:])
-        blocks = numpy.append(blocks, values)
-
-addToClipBoard(str(blocks))
-
 class BlocksArray:
     def __init__(self):
-        self.blocksArray = []
+        self.blocksArray = numpy.array([])
+
+    def rgbValues(imageName: str):
+        im = Image.open("block/" + imageName, "r")
+        pixelValues = list(im.getdata())
+        blockRGBValues = numpy.array([], dtype = numpy.uint8)
+        for rgba in pixelValues:
+            blockRGBValues = numpy.append(blockRGBValues, rgba[0])
+            blockRGBValues = numpy.append(blockRGBValues, rgba[1])
+            blockRGBValues = numpy.append(blockRGBValues, rgba[2])
+        return blockRGBValues
+
+    def getRGB(self):
+        blocks = numpy.empty((0, 0), dtype = numpy.uint8)
+
+        directory = "./block"
+        for filename in glob.iglob(f'{directory}/*'):
+            if filename.endswith(".png"):
+                values = rgbValues(filename[8:])
+                blocks = numpy.append(blocks, values)
+
+        self.blocksArray = blocks
