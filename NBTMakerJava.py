6790880d16368d4bba0f96d28bdc1f4b43e204f8
slickOnGit <milesdz@icloud.com>
2022-08-02 14:11:49 -0700

    java should work now

--- NBTMakerJava.py
+++ NBTMakerJava.py
@@ -1,5 +1,3 @@
-from ctypes import Structure
-
 from numpy import integer
 from Block import Block
 # pip install nbt
@@ -7,41 +5,54 @@ from nbtlib import *
 import numpy as np
 
 class NBTMakerJava:
-    """def __init__(self, blocks, shape):
-        self.shape = shape
-        # make block palette from block types in input array, add air at the beginning
+    def __init__(self, blocks, shape):
         self.palette = np.append('minecraft:air', np.unique(["minecraft:" + block.type for block in blocks]))
         self.palette = self.palette.tolist()
-        # turn blocks set into array, substituting the block name for its index in the palette using np.where,
-        # which for some reason returns it as [[index]]
-        blocksArr = [[block.x, block.y, block.z, self.palette.index("minecraft:" + block.type)] for block in blocks]
-        # all air to start
-        self.blockIndices = [0] * np.prod(shape)
-        # add in blocks
-        for block in blocksArr:
-            # get index by finding where it is along the 1d list based off of shape and coords
-            self.blockIndices[block[0]*shape[1]*shape[2] + block[1]*shape[2] + block[2]] = block[3]
-        
-        # blockIndices has a second layer of all -1s, sometimes these are other numbers if blocks need a special value
-        self.blockIndices = [self.blockIndices, [-1] * np.prod(shape)]"""
+
+        self.shape = shape
+        self.blocks = blocks
 
     def makeNBT(self, filename):
-        new_structure = File({
-            "foo": String("bar"),
-            "spam": IntArray([1, 2, 3]),
-            "egg": List[String](["hello", "world"])
-        })
-        new_structure.save("new_structure.nbt", byteorder = "big")
 
-        """class StructureFile(File, Structure):
+        Structure = schema("Structure", {
+            "DataVersion": Int,
+            "author": String,
+            "size": List[Int],
+            "palette": List[schema("State", {
+                "Name": String,
+                "Properties": Compound,
+            })],
+            "blocks": List[schema("Block", {
+                "state": Int,
+                "pos": List[Int],
+                "nbt": Compound,
+            })],
+            "entities": List[schema("Entity", {
+                "pos": List[Double],
+                "blockPos": List[Int],
+                "nbt": Compound,
+            })],
+        })
+
+        class StructureFile(File, Structure):
             def __init__(self, structure_data=None):
                 super().__init__(structure_data or {})
-                self.gzipped = False
+                self.gzipped = True
             @classmethod
-            def load(cls, filename, gzipped=False):
+            def load(cls, filename, gzipped=True):
                 return super().load(filename, gzipped)
 
-        structure_file = StructureFile(new_structure)
-        structure_file.save('new_structure.nbt', byteorder='big')  # you can load it in a minecraft world!"""
+        new_structure = Structure({
+            "DataVersion": 2730,
+            "size": self.shape,
+            "palette": [
+                {"Name": self.palette[i]} for i in range(len(self.palette))
+            ],
+            "blocks": [
+                {"pos": [block.x, block.y, block.z], "state": self.palette.index("minecraft:" + block.type)} for block in self.blocks
+            ],
+            "entities": []
+        })
 
-NBTMakerJava().makeNBT("foo")
+        structure_file = StructureFile(new_structure)
+        structure_file.save(filename, byteorder="big")
